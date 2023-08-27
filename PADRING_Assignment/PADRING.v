cb5cf7f78a848841441886e6efdeb1b9ec726a01
devchadha-jmi <chadhadev2002@gmail.com>
2023-08-27 20:14:57 +0530

    Updated Padring

--- PADRING_Assignment/PADRING.v
+++ PADRING_Assignment/PADRING.v
@@ -16,16 +16,39 @@ module PADRING #(parameter gpio_width = 15,
     // TMode PADBIDIR
     wire Y_TMode; 
     PADBIDIR Tmode (.PAD(pad_tm), .IE(1'b1), .OE(1'b0), .Y(Y_TMode), .A(1'b0));
-    
+
+    // Declaring Nets
+    reg [gpio_width-1:0] out_t_net;
+    reg [gpio_width-1:0] A_t_net;
+    reg [gpio_width-1:0] pdr_OUT_net;
+
     // Pads instantiated
     genvar i;
     generate 
         for (i = 0; i < gpio_width; i++)
         begin : dut_pad
-          PADBIDIR gpio (.PAD(gpio[i]), .IE(~Y_pad_ctrl), .OE(Y_pad_ctrl), .Y(pdr_OUT[i]), .A((pdr_IN[i] & ~Y_TMode) | (in_t[i] & Y_TMode)));
+            if (Y_TMode)
+            begin
+                out_t_net[i] = pdr_OUT_net[i];
+                A_t_net[i]   = in_t[i]
+            end
+            else
+            begin
+                out_t_net[i] = 1'b0;
+                A_t_net[i]   = pdr_IN[i];
+            end
+
+          PADBIDIR gpio (
+              .PAD(gpio[i]), 
+              .IE(~Y_pad_ctrl), 
+              .OE(Y_pad_ctrl), 
+              .Y(pdr_OUT_net[i]),
+              .A(A_t_net[i])
+          );
         end
     endgenerate
 
-  assign out_t[14:0] = pdr_OUT[data_width-1 : 0] & Y_TMode;
+    assign out_t  [gpio_width-1:0] = out_t_net  [gpio_width-1:0];
+    assign pdr_OUT[gpio_width-1:0] = pdr_OUT_net[gpio_width-1:0];
 
 endmodule
